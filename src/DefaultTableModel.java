317585f8faf7818fc0d6030ada951bc9b022291b
Adristiftitah <adristiiftitahyuniar@gmail.com>
2017-03-09 12:26:47 -1200

    MODUL10

---
+++ src/DefaultTableModel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Adristi Iftitah
+ */
+class DefaultTableModel {
+
+    DefaultTableModel(Object object, String[] kolom) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void addRow(String[] data) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/KoneksiDB.java
@@ -0,0 +1,24 @@
+
+import java.sql.ResultSet;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Adristi Iftitah
+ */
+public class KoneksiDB {
+
+    static int execute(String SQL) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static ResultSet executeQuery(String SQL) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
